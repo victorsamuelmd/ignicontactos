ef4972ebbffd734a9c641ba52914c8eadda7a0af
Victor Samuel Mosquera Artamonov <victorsamuelmd@gmail.com>
2016-06-17 07:01:13 -0500

    Modificar estructura de la carpeta

--- contacto.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include_once('data_types.php');
-
-
-if(isset($_GET['id'])){
-    echo $contactos[$_GET['id']]->nombres;
-}
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION['username'])){
-    header('Location: /contactos/contactos.php');
-} else {
-    header('Location: /contactos/autenticar.php');
-}
