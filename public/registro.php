001c0ae45cac93e52d82e5c6f806656e020e575a
victorsamuelmd <victor@igniweb.com>
2016-06-28 11:46:45 -0500

    Validar la informacion antes de crear el nuevo usuario

--- public/registro.php
+++ public/registro.php
@@ -34,16 +34,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         <div class="pure-u-1-3">
             <h1>Registrarse</h1>
             <form action="registro.php" method="post" class="pure-form pure-form-stacked">
-                <input type="text" placeholder="Nombre Usuario" name="username">
-                <input type="email" placeholder="Email" name="email">
-                <input type="password" placeholder="Password" name="password">
+                <input type="text" placeholder="Nombre Usuario" name="username" pattern="[a-zA-Z0-9]{6,50}" id="username">
+                <input type="email" placeholder="Email" name="email" id="email">
+
+                <input type="password" 
+                       id="password"
+                       placeholder="Password"
+                       name="password"
+                       pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$">
+
                 <input type="submit" value="Registrarse" class="pure-button pure-button-primary">
             </form>
         <?php if (isset($_GET['error'])) { ?>
-        <p class="error"><?php echo $_GET['error']; ?></p>
+            <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
         </div>
         <div class="pure-u-1-3">
         </div>
+    <script>
+'use strict';
+
+var usernameInput = document.getElementById('username');
+usernameInput.oninvalid = function (event) {
+    event.target.setCustomValidity('El nombre de usuario debe tener solo caracteres alfanuméricos, mínimo 6 caracteres');
+}
+var passwordInput = document.getElementById('password');
+passwordInput.oninvalid = function (event) {
+    event.target.setCustomValidity('La contraseña debe contener al menos 8 caracteres, con al menos una mayúscula, una letra minúscula, al menos un numero o un caracter especial');
+}
+</script>
     </body>
 </html>
